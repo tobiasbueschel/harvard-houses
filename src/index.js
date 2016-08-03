a310526362299ffa3421060144ada7e9af5f67d5
Tobias Büschel <tobias.buschel.15@ucl.ac.uk>
2016-08-03 00:20:45 -0400

    feat(random): Add ability to get an array of harvard house names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 var uniqueRandomArray = require('unique-random-array');
 var harvardHouses = require('./harvard-houses.json');
+var getRandomHouse = uniqueRandomArray(harvardHouses);
+
+const random = number => {
+	if (number === 'undefined') {
+		return getRandomHouse();
+	} else {
+		var getRandomHouses = [];
+		for (var i = 0; i < number; i++){
+			getRandomHouses.push(getRandomHouse());
+		}
+		// console.log(getRandomHouses);
+		return getRandomHouses;
+	}
+}
 
 module.exports = {
 	all: harvardHouses,
-	random: uniqueRandomArray(harvardHouses)
+	random: random
 };
